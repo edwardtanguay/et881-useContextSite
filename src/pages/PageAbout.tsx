b4481023249d832dd2de71309540d73afb79f7f5
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-26 15:26:57 +0200

    skills being used on all pages

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,13 +1,19 @@
-import { useContext } from "react"
-import { AppContext } from "../AppContext"
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 export const PageAbout = () => {
-	const { handleChangeAppTitle } = useContext(AppContext);
+	const { handleChangeAppTitle, skills } = useContext(AppContext);
 
 	return (
 		<>
-		<p>This is the about page.</p>
-		<button className="bg-slate-300 rounded px-2 py-1 mt-3" onClick={handleChangeAppTitle}>Change title</button>
+			<p>This is the about page.</p>
+			<button
+				className="bg-slate-300 rounded px-2 py-1 mt-3 mb-3"
+				onClick={handleChangeAppTitle}
+			>
+				Change title
+			</button>
+			<p>Our next skill will be #{skills.length + 1}.</p>
 		</>
-	)
-}
+	);
+};
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -2,9 +2,9 @@ import { useContext } from "react"
 import { AppContext } from "../AppContext"
 
 export const PageWelcome = () => {
-	const { appTitle } = useContext(AppContext);
+	const { appTitle, skills } = useContext(AppContext);
 
 	return (
-		<p>Welcome to our site called {appTitle}.</p>
+		<p>Welcome to our site called {appTitle}, we currently have {skills.length} skills.</p>
 	)
 }
