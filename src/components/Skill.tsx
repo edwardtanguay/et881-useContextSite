9dcb976c1c501d5358af82dc8c3e123be76e3816
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-26 15:35:08 +0200

    trashcan icon added

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,4 +1,5 @@
 import { ISkill } from "../types";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 interface IProps {
 	skill: ISkill;
@@ -6,7 +7,9 @@ interface IProps {
 
 export const Skill = ({ skill }: IProps) => {
 	return <div className="bg-slate-600 w-[20rem] p-3 rounded">
-		<div className="text-xl text-orange-400">{skill.name}</div>
+		<div className="text-xl text-orange-400 flex justify-between">{skill.name}
+		<FaRegTrashCan className="text-red-500 mt-2 text-[1rem]" />
+		</div>
 		<div className="text-slate-300 italic">{skill.description}</div>
 	</div>;
 };
