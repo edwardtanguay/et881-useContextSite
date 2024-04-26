559b9ed5ffbe1886f4762feff1a3d2cc06be089f
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-26 14:59:23 +0200

    shows 0 skills

--- src/pages/PageSkills.tsx
+++ src/pages/PageSkills.tsx
@@ -1,5 +1,10 @@
+import { useContext } from "react"
+import { AppContext } from "../AppContext"
+
 export const PageSkills = () => {
+	const { skills } = useContext(AppContext);
+
 	return (
-		<p>This is the skills page.</p>
+		<p>There are {skills.length} skills.</p>
 	)
 }
