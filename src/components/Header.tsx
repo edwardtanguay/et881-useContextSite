87df10857b027ea773b305066d77149aca55c65e
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-26 14:10:40 +0200

    appContext in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { Nav } from "./Nav";
+import { AppContext } from "../AppContext";
 
 export const Header = () => {
+	const { appTitle } = useContext(AppContext);
+
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
+			<h1 className="text-3xl mb-3 text-slate-800">{appTitle}</h1>
 			<Nav/>
 		</>
 	);
